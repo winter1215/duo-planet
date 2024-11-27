aa6de0c9f5fda7ef4d33638e2ad74d1551c4e635
winter <2436197699@qq.com>
2024-11-27 21:07:50 +0800

    refactor: 修改文档

--- duo/sql/duoduo.sql
+++ duo/sql/duoduo.sql
@@ -42,38 +42,3 @@ create table diary
 )
     comment '日记' collate = utf8mb4_unicode_ci
                    row_format = DYNAMIC;
-
-create table ums_post_favour
-(
-    id         bigint auto_increment comment 'id'
-        primary key,
-    postId     bigint                             not null comment '帖子 id',
-    userId     bigint                             not null comment '创建用户 id',
-    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间'
-)
-    comment '帖子收藏' row_format = DYNAMIC;
-
-create index idx_postId
-    on ums_post_favour (postId);
-
-create index idx_userId
-    on ums_post_favour (userId);
-
-create table ums_post_thumb
-(
-    id         bigint auto_increment comment 'id'
-        primary key,
-    postId     bigint                             not null comment '帖子 id',
-    userId     bigint                             not null comment '创建用户 id',
-    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间'
-)
-    comment '帖子点赞' row_format = DYNAMIC;
-
-create index idx_postId
-    on ums_post_thumb (postId);
-
-create index idx_userId
-    on ums_post_thumb (userId);
-
